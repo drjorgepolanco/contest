371eea10f19383b2fb6289c8045794fa03e53c80
drjorgepolanco <dr.jorgepolanco@gmail.com>
2014-01-10 16:46:50 -0600

    Install foundation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Contest::Application.routes.draw do
-  get "static_pages/home"
+  root :to => "static_pages#home"
   get "static_pages/about"
   get "static_pages/help"
   get "static_pages/contact"
