e7645273f42b0a8aabe087bdaf39b068462cf34b
drjorgepolanco <dr.jorgepolanco@gmail.com>
2014-01-10 16:35:12 -0600

    Finish Static Pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,66 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Talentcrops'" do
+      visit '/static_pages/home'
+      expect(page).to have_content("Talentcrops")
+    end
+
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Talentcrops")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
+
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Talentcrops | Help")
+    end
+
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Talentcrops | About Us")
+    end
+
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+
+    it "should have the title 'Contact Us" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Talentcrops | Contact Us")
+    end
+  
+  end
+
+end
