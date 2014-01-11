04eef42144a43b889f35711b009658324fd71e15
drjorgepolanco <dr.jorgepolanco@gmail.com>
2014-01-10 22:19:31 -0600

    Add basic User model with secure password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,19 @@ require 'spec_helper'
 
 describe User do
 
-  before {@user = User.new(first_name: "Jorge", last_name: "Cukito", email: "drjorgepolanco@gmail.com", date_of_birth: 07/31/1983)}
-
+  before {@user = User.new(first_name: "Jorge", last_name: "Cukito", email: "drjorgepolanco@gmail.com", 
+                            password: "foobar", password_confirmation: "foobar")}
+  # , date_of_birth: 07/31/1983
   subject {@user}
 
   it {should respond_to(:first_name)}
   it {should respond_to(:last_name)}
   it {should respond_to(:email)}
-  it {should respond_to(:date_of_birth)}
+  #it {should respond_to(:date_of_birth)}
+  it {should respond_to(:password_digest)}
+  it {should respond_to(:password)}
+  it {should respond_to(:password_confirmation)}
+  it {should respond_to(:authenticate)}
 
   it {should be_valid}
 
@@ -28,10 +33,10 @@ describe User do
     it { should_not be_valid }
   end
 
-  describe "when date of birth is not present" do
-    before { @user.date_of_birth = '' }
-    it { should_not be_valid }
-  end
+    # describe "when date of birth is not present" do
+    #   before { @user.date_of_birth = '' }
+    #   it { should_not be_valid }
+    # end
 
   describe "when first name is too long" do
     before { @user.first_name = "a" * 51 }
@@ -72,5 +77,39 @@ describe User do
     end
 
     it {should_not be_valid}
+  end
+
+  describe "when password is not present" do
+    before do
+      @user = User.new(first_name: "Jorge", last_name: "Polanco", email: "drjorgepolanco@gmail.com",
+                        password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid}
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid}
+  end
+
+  describe "when a password is too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it {should_not be_valid}
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
+
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
+    end
   end
 end
