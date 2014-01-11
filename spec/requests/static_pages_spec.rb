9af82bae4f92b529e41bd987a6b5ce86f6415807
drjorgepolanco <dr.jorgepolanco@gmail.com>
2014-01-10 18:33:12 -0600

    Refactor tests and Add RSpec utilities

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,65 +2,35 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the h1 'Talentcrops'" do
-      visit root_path
-      expect(page).to have_content("Talentcrops")
-    end
-
-    it "should have the base title" do
-      visit root_path
-      expect(page).to have_title("Talentcrops")
-    end
-
-    it "should not have a custom page title" do
-      visit root_path
-      expect(page).not_to have_title('| Home')
-    end
-
+    it { should have_content('Talentcrops') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the h1 'Help'" do
-      visit help_path
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit help_path
-      expect(page).to have_title("Talentcrops | Help")
-    end
-
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the h1 'About Us'" do
-      visit about_path
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit about_path
-      expect(page).to have_title("Talentcrops | About Us")
-    end
-
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact page" do
+    before { visit contact_path }
 
-    it "should have the h1 'Contact Us'" do
-      visit contact_path
-      expect(page).to have_content('Contact Us')
-    end
-
-    it "should have the title 'Contact Us" do
-      visit contact_path
-      expect(page).to have_title("Talentcrops | Contact Us")
-    end
-  
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact Us')) }  
   end
 
 end
