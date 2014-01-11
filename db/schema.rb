0e2879708c0254f7f98a17a3cab73c7b6b5b8b4e
drjorgepolanco <dr.jorgepolanco@gmail.com>
2014-01-10 20:31:23 -0600

    Add Index to Users Email, write tests, Add validation to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140111022735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "profile_pic"
+    t.date     "date_of_birth"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "email"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
-  subject { page }
+  subject {page}
 
   shared_examples_for "all static pages" do
-    it { should have_selector('h1', text: heading) }
-    it { should have_title(full_title(page_title)) }
+    it {should have_selector('h1', text: heading)}
+    it {should have_title(full_title(page_title))}
   end
 
   describe "Home page" do
-    before { visit root_path }
+    before {visit root_path}
     let(:heading)     {'Talentcrops'}
     let(:page_title)  {''}
 
@@ -19,7 +19,7 @@ describe "Static Pages" do
   end
 
   describe "Help page" do
-    before { visit help_path }
+    before {visit help_path}
     let(:heading)     {'Help'}
     let(:page_title)  {''}
 
@@ -27,7 +27,7 @@ describe "Static Pages" do
   end
 
   describe "About page" do
-    before { visit about_path }
+    before {visit about_path}
     let(:heading)     {'About Us'}
     let(:page_title)  {''}
 
@@ -35,11 +35,17 @@ describe "Static Pages" do
   end
 
   describe "Contact page" do
-    before { visit contact_path }
+    before {visit contact_path}
     let(:heading)     {'Contact Us'}
     let(:page_title)  {''}
 
     it_should_behave_like "all static pages"     
   end
 
+  # before {visit root_path}
+  # it {should have_title(full_title('Help'))}
+  # it {should have_title(full_title('About Us'))}
+  # it {should have_title(full_title('Contact Us'))}
+  # it {should have_title(full_title('Sign Up'))}
+  
 end
